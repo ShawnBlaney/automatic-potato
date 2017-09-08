ccba777579473e12796bd3bc10517c7a6bfcc7d1
ShawnBlaney <shawn.blaney@hotmail.ca>
2017-09-08 12:27:02 -0400

    initial commit
    first commit for android studio project

---
+++ TipCalculator/app/src/main/java/com/example/a1543950/tipcalculator/TipActivityFragment.java
@@ -0,0 +1,26 @@
+package com.example.a1543950.tipcalculator;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TipActivityFragment extends Fragment {
+
+    public TipActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_tip, container, false);
+
+        root.findViewById()
+
+        return root;
+    }
+}
